ad0810844f52bd37fc26e613334e653747669ac8
Михаил <mihail@MacBook-Air-Mihail.local>
2025-12-11 22:49:55 +0300

    add api

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ import { initSorting } from './components/sorting.js';
 import { initFiltering } from './components/filtering.js';
 
 // Исходные данные используемые в render()
-const {data, ...indexes} = initData(sourceData);
+const API = initData(sourceData);
 
 /**
  * Сбор и обработка полей из таблицы
@@ -36,16 +36,19 @@ function collectState() {
  * Перерисовка состояния таблицы при любых изменениях
  * @param {HTMLButtonElement?} action
  */
-function render(action) {
+async function render(action) {
     let state = collectState(); // состояние полей из таблицы
-    let result = [...data]; // копируем для последующего изменения
-    // @todo: использование
+    let query = {}; // здесь будут формироваться параметры запроса
+    // другие apply*
+    // result = applySearching(result, state, action);
+    // result = applyFiltering(result, state, action);
+    // result = applySorting(result, state, action);
+    query = applyPagination(query, state, action); // обновляем query
 
-    result = applyFiltering(result, state, action);
-    result = applySorting(result, state, action);
-    result = applyPagination(result, state, action);
+    const { total, items } = await api.getRecords(query); // запрашиваем данные с собранными параметрами
 
-    sampleTable.render(result)
+    updatePagination(total, query); // перерисовываем пагинатор
+    sampleTable.render(items);
 }
 
 const sampleTable = initTable({
@@ -61,25 +64,19 @@ const applySorting = initSorting([
     sampleTable.header.elements.sortByTotal
 ]);
 
-const applyFiltering = initFiltering(sampleTable.filter.elements, {
-    searchBySeller: indexes.sellers
-});
+// const applyFiltering = initFiltering(sampleTable.filter.elements, {
+//     searchBySeller: indexes.sellers
+// });
 
-const applyPagination = initPagination(
-    sampleTable.pagination.elements, 
-    (el, page, isCurrent) => {
-    const input = el.querySelector('input');
-    const label = el.querySelector('span');
+const {applyPagination, updatePagination} = initPagination();
+
     
-    input.value = page;
-    input.checked = isCurrent;
-    label.textContent = page;
-
-    return el;
-});
-
-
 const appRoot = document.querySelector('#app');
 appRoot.appendChild(sampleTable.container);
 
-render();
+async function init() {
+    const indexes = await API.getIndexes()
+}
+
+init().then(render);
+
